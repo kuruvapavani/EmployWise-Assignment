f1a03d50bf4cbf45de17d3d3a470916e403b0e39
Kuruva Pavani <pavanikuruva2109@gmail.com>
2025-03-28 18:32:34 +0530

    complete signup component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
-import ErrorPage from './pages/ErrorPage'
-import HomePage from './pages/HomePage'
-import LoginPage from './pages/LoginPage'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUp from "./components/signup/SignUp";
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path='*' element={<ErrorPage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
